7b63ac5343c3e7602fce0a71709938274c980124
Rafael Souza <rafaeljosegomes25@gmail.com>
2024-01-27 12:29:08 -0300

    add forgot-password views

--- authentication/api/viewsets.py
+++ authentication/api/viewsets.py
@@ -1,6 +1,14 @@
 from rest_framework import generics, permissions, status
 from rest_framework.response import Response
 from django.contrib.auth.hashers import make_password
+from django.contrib.auth.tokens import default_token_generator
+from django.utils.encoding import force_bytes, force_text
+from django.utils.http import urlsafe_base64_encode, urlsafe_base64_decode
+from rest_framework.views import APIView
+from rest_framework_simplejwt.tokens import RefreshToken
+from django.contrib.sites.shortcuts import get_current_site
+from django.template.loader import render_to_string
+from django.core.mail import EmailMessage
 
 from authentication.models import User
 from .serializers import UserSerializer
@@ -39,3 +47,46 @@ class CreateUserView(generics.CreateAPIView):
             status=status.HTTP_201_CREATED,
             headers=headers,
         )
+
+
+class PasswordResetView(APIView):
+    def post(self, request):
+        email = request.data.get('email')
+        user = User.objects.filter(email=email).first()
+
+        if user:
+            current_site = get_current_site(request)
+            subject = 'Reset Your Password'
+            message = render_to_string('password_reset_email.html', {
+                'user': user,
+                'domain': current_site.domain,
+                'uid': urlsafe_base64_encode(force_bytes(user.pk)),
+                'token': default_token_generator.make_token(user),
+            })
+            to_email = user.email
+            email = EmailMessage(subject, message, to=[to_email])
+            email.send()
+            return Response({'detail': 'Password reset email sent.'}, status=status.HTTP_200_OK)
+        else:
+            return Response({'detail': 'No user found with that email address.'}, status=status.HTTP_404_NOT_FOUND)
+
+
+class PasswordResetConfirmView(APIView):
+    def post(self, request, uidb64, token):
+        try:
+            uid = force_text(urlsafe_base64_decode(uidb64))
+            user = User.objects.get(pk=uid)
+        except (TypeError, ValueError, OverflowError, User.DoesNotExist):
+            user = None
+
+        if user and default_token_generator.check_token(user, token):
+            new_password = request.data.get('new_password')
+            user.set_password(new_password)
+            user.save()
+
+            refresh = RefreshToken.for_user(user)
+            access_token = str(refresh.access_token)
+
+            return Response({'access_token': access_token}, status=status.HTTP_200_OK)
+        else:
+            return Response({'detail': 'Invalid token or user not found.'}, status=status.HTTP_400_BAD_REQUEST)
